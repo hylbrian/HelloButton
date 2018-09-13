1082d5d95cfb8e7d8d5c4709e4193665991f586a
Brian Hyland <39333309+hylbrian@users.noreply.github.com>
2018-09-13 16:57:11 +0100

    View update with buttons

--- HelloButton/app/src/main/java/com/example/brianhyland/hellobutton/MainActivity.java
+++ HelloButton/app/src/main/java/com/example/brianhyland/hellobutton/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.brianhyland.hellobutton;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mMessageTextView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // this method is called when the app launches
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //code you add goes after setContentView
     }
 }
